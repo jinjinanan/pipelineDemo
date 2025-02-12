b2e0d96619e1e72d8b01f7c486ad77dfeb71a29d
clb <79861>
2025-02-12 17:51:17 +0800

    save

---
+++ pipelineProject/pipelineProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pipelineProject
+//
+//  Created by 陈林波 on 2025/2/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pipelineProject/pipelineProject/pipelineProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  pipelineProjectApp.swift
+//  pipelineProject
+//
+//  Created by 陈林波 on 2025/2/12.
+//
+
+import SwiftUI
+
+@main
+struct pipelineProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
